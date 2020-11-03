0c234349d114c47a575e398220b05501f20fd947
Hampton Rogers <rogershampton22@gmail.com>
2020-11-03 15:45:45 -0600

    Add project files.

---
+++ ClassesPractice/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesPractice
+{
+    public class Car
+    {
+        //Constructors
+        //Default Constructor
+        public Car() //- a speacial member method that has the same name as
+        {
+
+        }
+
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        //Fields
+        //Properties
+        //Methods
+
+            //Create a Make property of type string that is public
+        public string Make { get; set; } //get = read and set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+    }
+}
---
+++ ClassesPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassesPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dot notation 
+            Car myCar = new Car();
+            myCar.Make = "Acura";
+            myCar.Model = "RDX";
+            myCar.Year = 2014;
+            Console.WriteLine($"{myCar.Make}, {myCar.Model}, {myCar.Year}");
+
+            //object initializer
+            //var honda = new Car()
+            //
+            //    Make = "Honda",
+            //    Model = "Civic",
+            //    Year = 2010
+            //};
+
+            //Constructors
+            //var ford = Car("Ford", "Explorer", 2003);
+        }
+    }
+}
